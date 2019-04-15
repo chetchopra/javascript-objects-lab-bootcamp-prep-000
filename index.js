aaf604e02cd0ff20fb78d268a088f017dc059150
bustling-maker-4835 <chopra.chet@gmail.com>
2019-04-15 20:48:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  o2 = object.slice(0)
-  o2[key] = value;
-  return o2;
+  var newo = Object.assign({}, oject);
+  newo[key] = value;
+  return newo;
 }
